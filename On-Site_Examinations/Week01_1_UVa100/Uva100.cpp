77224d627cad5232413108cde74e743e5f80fbda
Hans Chen <hanschen31310@gmail.com>
2026-04-13 00:29:48 +0800

    fix(uva100): handle repeated input pairs correctly
    Process input until EOF, preserve the original pair order in the output, and normalize the range only inside the cycle-length calculation.

--- On-Site_Examinations/Week01_1_UVa100/Uva100.cpp
+++ On-Site_Examinations/Week01_1_UVa100/Uva100.cpp
@@ -1,32 +1,31 @@
-//inclass01
 #include <iostream>
 
 using namespace std;
 
-	int Uva100(int a, int b) {
-		int minimum = min(a, b);
-		int maximum = max(a, b);
-		int max_count = 1;
+int Uva100(int a, int b) {
+	if (a > b) swap(a, b);
+	int max_count = 1;
 
-		for (int i = a; i <= b; i++) {
-			int count = 1;
-			int tmp = i;
+	for (int i = a; i <= b; i++) {
+		int count = 1;
+		int tmp = i;
 
-			while (tmp != 1) {
-				if (tmp % 2 == 0) tmp /= 2;
-				else tmp = 3 * tmp + 1;
-				count++;
-			}
-			max_count = max(count, max_count);
+		while (tmp != 1) {
+			if (tmp % 2 == 0) tmp /= 2;
+			else tmp = 3 * tmp + 1;
+			count++;
 		}
-		return max_count;
+		max_count = max(count, max_count);
 	}
+	return max_count;
+}
 
 int main() {
 	int a, b;
-	cin >> a >> b;
-	cout << a << " " << b << " ";
-	cout << Uva100(a, b) << endl;
+	while (cin >> a >> b) {
+		cout << a << " " << b << " ";
+		cout << Uva100(a, b) << endl;
+	};
 
 	return 0;
 }
