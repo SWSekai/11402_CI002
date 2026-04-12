dc3689dc4eddf04655da76c3344d69c2b5b19a8d
Hans Chen <hanschen31310@gmail.com>
2026-04-13 00:30:13 +0800

    refactor(onsite): clean up UVA helper-based solutions
    Extract helper routines for UVa10783 and UVa10170, simplify input handling, and keep the core judge logic easier to read and reuse.

--- On-Site_Examinations/Week03_1_UVa10783/UVa10783.cpp
+++ On-Site_Examinations/Week03_1_UVa10783/UVa10783.cpp
@@ -2,28 +2,25 @@
 
 using namespace std;
 
-int num;
+class Odd_Sum {
+public:
+	static int calculate_all_odd_sun(int a, int b) {
+		int ans = 0;
+		if (a > b) swap(a, b);
+		for (int i = a; i <= b; i++) (i % 2 != 0) ? ans += i : 0;
+
+		return ans;
+	}
+};
 
 int main() {
-	cin >> num;
-	int round = 1;
+	int round, case_num = 1;
+	cin >> round;
 
-	while (num) {
-		int a, b;
-		int ans = 0;
-		cin >> a >> b;
-
-		if (a > b) {
-			int tmp = a;
-			a = b;
-			b = tmp;
-		}
-
-		for (int i = a; i <= b; i++) {
-			if (i % 2 != 0) ans += i;
-		}
-		cout << "Case " << round << ": " << ans << endl;
-		num--;
-		round++;
+	while (round--){
+		int num1, num2;
+		cin >> num1 >> num2;
+		cout << "Case " << case_num << ": " << Odd_Sum::calculate_all_odd_sun(num1, num2) << endl;
+		case_num++;
 	}
 }
--- On-Site_Examinations/Week03_2_UVa10170/UVa10170.cpp
+++ On-Site_Examinations/Week03_2_UVa10170/UVa10170.cpp
@@ -2,12 +2,24 @@
 
 using namespace std;
 
-int initial_member;
-long long target_room;
-
-int ans = 0;
+class Infinite_room {
+public:
+	static long long get_room_group_size(int initial_member, long long target_room_number) {
+		long long current_archive_room = 0;
+		for(long long i = initial_member; i <= target_room_number; i++) {
+			current_archive_room += i;
+			if (current_archive_room >= target_room_number) {
+				return i;
+			}
+		}
+		return -1; // Should not reach here
+	}
+};
 
 int main() {
+	int initial_member;
+	long long target_room;
+
 	while (cin >> initial_member >> target_room) {
 		int current_archive_room = 0;
 		if (initial_member >= target_room) {
@@ -15,13 +27,7 @@ int main() {
 			continue;
 		}
 
-		for (int i = initial_member; i <= target_room; i++) {
-			current_archive_room += i;
-			if (current_archive_room >= target_room) {
-				cout << i << endl;
-				break;
-			}
-		}
+		cout << Infinite_room::get_room_group_size(initial_member, target_room) << endl;
 
 		//for (int end = sqrt(target_room)+1; end >= initial_member; end--) {
 		//	current_archive_room = ((end + initial_member) * ((end - initial_member)+1)) / 2;
