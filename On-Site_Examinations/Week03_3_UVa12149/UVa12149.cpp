c8ab23b27a3fca391fa1d35a52daf94dbf571040
Hans Chen <hanschen31310@gmail.com>
2026-04-13 16:27:03 +0800

    refactor: encapsulate UVa 12149 counting logic

--- On-Site_Examinations/Week03_3_UVa12149/UVa12149.cpp
+++ On-Site_Examinations/Week03_3_UVa12149/UVa12149.cpp
@@ -2,13 +2,11 @@
 
 using namespace std;
 
-int num;
-
-int main() {
-	while (cin >> num && num != 0) {
+class Feynman {
+public:
+	static int count(int num) {
 		if (num == 1) {
-			cout << 1 << endl;
-			continue;
+			return 1;
 		}
 
 		int ans = 0;
@@ -17,11 +15,18 @@ int main() {
 		for (int i = 1; i <= num; i++) {
 			inside_square = i;
 			int row_allow_num = 0;
-			for (int j = 1; j <= num - inside_square+1; j++) {
+			for (int j = 1; j <= num - inside_square + 1; j++) {
 				row_allow_num += 1;
 			}
-			ans += row_allow_num*row_allow_num;
+			ans += row_allow_num * row_allow_num;
 		}
-		cout << ans << endl;
+		return ans;
+	}
+};
+
+int main() {
+	int num;
+	while (cin >> num && num != 0) {
+		cout << Feynman::count(num) << endl;
 	}
 }
